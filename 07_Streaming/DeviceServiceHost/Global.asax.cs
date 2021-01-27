21f036c4e944b469c21448008b0b2d4fe110355c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-01-27 12:36:45 +0100

    Dependency Injection

---
+++ 07_Streaming/DeviceServiceHost/Global.asax.cs
@@ -0,0 +1,59 @@
+﻿using Autofac;
+using Autofac.Integration.Wcf;
+using Bogus;
+using DeviceServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace DeviceServiceHost
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<DeviceService>();
+            builder.RegisterType<FakeDeviceRepository>().As<IDeviceRepository>();
+            builder.RegisterType<DeviceFaker>().As<Faker<Device>>();
+
+            IContainer container = builder.Build();
+
+            AutofacHostFactory.Container = container;
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 08_DependencyInjection/DeviceServices/IDeviceService.cs
@@ -0,0 +1,117 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceServices
+{
+    [ServiceContract]
+    public interface IDeviceService
+    {
+        [OperationContract]
+        IEnumerable<Device> Get();
+        [OperationContract] 
+        Device GetById(int id);
+        [OperationContract] 
+        void Add(Device device);
+    }
+
+    public interface IDeviceRepository
+    {
+        IEnumerable<Device> Get();
+        Device Get(int id);
+        void Add(Device device);
+    }
+
+    public class DbDeviceRepository : IDeviceRepository
+    {
+        public void Add(Device device)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Device> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Device Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class DeviceFaker : Faker<Device>
+    {
+        public DeviceFaker()
+        {
+            RuleFor(p => p.Id, f => f.IndexFaker);
+            RuleFor(p => p.Name, f => f.Hacker.Phrase());
+            RuleFor(p => p.MacAddress, f => f.Internet.Mac());
+        }
+    }
+
+    public class FakeDeviceRepository : IDeviceRepository
+    {
+        private readonly ICollection<Device> devices = new List<Device>();
+
+        public FakeDeviceRepository(Faker<Device> faker)
+        {
+            devices = faker.Generate(100);
+        }
+        
+        public void Add(Device device)
+        {
+            devices.Add(device);
+        }
+
+        public IEnumerable<Device> Get()
+        {
+            return devices;
+        }
+
+        public Device Get(int id)
+        {
+            return devices.SingleOrDefault(d => d.Id == id);
+        }
+    }
+
+    public class DeviceService : IDeviceService
+    {
+        private readonly IDeviceRepository repository;
+
+        public DeviceService(IDeviceRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Add(Device device)
+        {
+            repository.Add(device);
+
+            Trace.WriteLine($"Added {device.Name}");
+        }
+
+        public IEnumerable<Device> Get()
+        {
+            return repository.Get();
+        }
+
+        public Device GetById(int id)
+        {
+            return repository.Get(id);
+        }
+    }
+
+    public class Device
+    {
+        public int Id { get; set; }
+        public string MacAddress { get; set; }
+        public string Name { get; set; }
+    }
+}
