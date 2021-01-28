fc2afa2d59e83d6526c14064d84741d8bb9a2839
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-01-28 08:28:19 +0100

    NET Core Client

---
+++ HelloServiceDotnetCoreConsoleClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloServiceDotnetCoreConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+          //  ServiceReference1.ServiceClient client = new ServiceReference1.ServiceClient();
+            
+        }
+    }
+}
